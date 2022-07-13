2ec0f469932f0ca0d3475d8b1165b4c31065fc90
Ahmed Selmi <ahmedselmi939@gmail.com>
2022-07-13 02:04:47 +0100

    Initial commit

---
+++ src/main/kotlin/rsk/Main.kt
@@ -0,0 +1,19 @@
+
+/*fun  rsk.rsk.main ()
+{
+  rsk.rsk.display()
+}
+fun rsk.rsk.display()
+{
+    println("Hello world!")
+}*/
+fun main(args:Array<String>){
+display("Hello World!")
+}
+fun display(message:String):Boolean
+{
+    println(message)
+    return true
+}
+
+
---
+++ src/main/kotlin/rsk/func.kt
@@ -0,0 +1,8 @@
+package rsk
+
+fun main(args:Array<String>)
+{
+    println(max(1,2))
+
+}
+fun max(a:Int ,b:Int) : Int = if (a>b) a else b
